3d1d99bcf14e85ba9298d562d896261c49cb8aa1
Snehanshu Phukon <snehanshuphukan@gmail.com>
2020-04-19 07:52:11 +0530

    ceaser cypher implemented

---
+++ ceaser.js
@@ -0,0 +1,14 @@
+function ceaser (text = '', key = 5) {
+    let ceaserText = text.split('').map(t => getCeaser(t, key));
+    return ceaserText.join('');
+}
+
+function getCeaser(t, key = 5) {
+    let charSet = 'abcdedghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+    if (!charSet.includes(t)) return t;
+    let newKey = charSet.indexOf(t) + key;
+    if (newKey >= charSet.length) {
+        newKey = newKey % charSet.length;
+    }
+    return charSet[newKey];
+}
