a035d04dce72cef6438d267d534c9c0439407164
Snehanshu Phukon <snehanshuphukan@gmail.com>
2020-04-26 13:43:12 +0530

    playfair

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ $(function () {
     var ceaser_result = ceaser(text, key);
     $("#ceaser_result").text(ceaser_result);
   });
+
+  $("#pf_enc_btn").click(function (e) {
+    e.preventDefault();
+    var text = $("#pf_text").val();
+    var key = $("#pf_key").val();
+    var pf_result = playfair.encipher(text, key);
+    $("#pf_enctxt").val(pf_result);
+  });
+
+  $("#pf_denc_btn").click(function (e) {
+    e.preventDefault();
+    var text = $("#pf_enctxt").val();
+    var key = $("#pf_key").val();
+    var pf_result = playfair.decipher(text, key);
+    $("#pf_text").val(pf_result);
+  });
 });
